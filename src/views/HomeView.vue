f42551adcfff67bb7d863a490d0c84f80c7c1a02
Holden <Holdenkh@gmail.com>
2022-11-21 18:03:24 +0200

    testisin veits asju

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,92 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <div id="flex_container">
+
+
+
+<div id = "main_section">
+    <aside class = "sidebar">
+        <!--boxis on the sides-->
+    </aside>
+
+    <main> <!--this is where all the posts go-->
+        <article class="post">
+
+            <div class = "post_header">
+                <img class = "icon" src="res/images/me.png" alt="profiil">
+                <p> Oct 22, 2022</p>
+
+            </div>
+            <img style="max-width: 60% ;" src="res/images/postitus_1.jpg" alt="pilt">
+            <p> Making something cool out of my food, how do you like it? </p>
+            <img class  = "icon" src="res/images/like_button.jpg" alt="like button">
+            
+        </article>
+
+        <article class = "post">
+
+            <div class = "post_header">
+                <img class = "icon" src="res/images/me.png" alt="profiil">
+                <p> Oct 23, 2022</p>
+            </div>
+
+            <img style="max-width: 60% ;" src="res/images/postitus_2.jpg" alt="pilt">
+            <p>Everythong becomes much clearer when you bring it into focus dont you think? </p>
+            <img class  = "icon" src="res/images/like_button.jpg" alt="like button">
+        </article>
+
+        <article class = "post">
+            <div class = "post_header">
+                <img class = "icon" src="res/images/me.png" alt="profiil">
+                <p> Oct 22, 2022</p>
+
+            </div>
+            <p> Does anybody have a good tent that I colud lend would be really thnkful! </p>
+            <img class  = "icon" src="res/images/like_button.jpg" alt="like button">
+        </article>
+
+        <article class = "post">
+            <div class = "post_header">
+                <img class = "icon" src="res/images/me.png" alt="profiil">
+                <p> Oct 22, 2022</p>
+
+            </div>
+            <p> “The best and most beautiful things in the world cannot be seen or even touched. They must be felt with the heart”
+                ― Helen Keller </p>
+            <img class  = "icon" src="res/images/like_button.jpg" alt="like button">
+        </article>
+
+        <article class = "post">
+            <div class = "post_header">
+                <img class = "icon" src="res/images/me.png" alt="profiil">
+                <p> Oct 22, 2022</p>
+
+            </div>
+            <img style="max-width: 60% ;" src="res/images/postitus_5.jpg" alt="pilt">
+            <p> Shes a cutie isnt she :)? </p>
+            <img class  = "icon" src="res/images/like_button.jpg" alt="like button">
+        </article>
+
+
+    </main>
+
+    <aside class = "sidebar">
+        <!--boxis on the sides-->
+    </aside>
+</div>
+
+<footer>
+    footer
+</footer>
+
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
