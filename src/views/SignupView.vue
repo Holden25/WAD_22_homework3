a1864fadf2dda3180f777e9910355bea2100554d
ReenaSeeba <reena.seeba@gmail.com>
2022-11-22 21:51:13 +0200

    Signup form on signuo page

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -3,7 +3,27 @@
 
     <Header/>
 
-    <div>This is a signup page</div>
+    <div>This is a signup page
+    <form @submit.prevent="handleSubmit">
+            <lable>Email: </lable>
+            <input type="email" v-model="email" required>
+            <div v-if="passwordError" class="error">{{passwordError}} </div>
+            <div v-if="passwordError2" class="error">{{passwordError2}} </div>
+            <div v-if="passwordError6" class="error">{{passwordError6}} </div>
+            <br>
+            <lable>Password:</lable>
+            <input type="password" v-model="password" required>
+
+            <div class ="button">
+                <button class="submit" type ="submit">Signup</button>
+            </div>
+            
+    </form>
+
+    <p> Email: {{ email }} </p>
+    <p> Password: {{ password }} </p>
+
+</div>
 
 </template>
 
@@ -13,7 +33,46 @@
 export default {
     name : "SignupView",
 
-    components: {Header}
+    components: {Header},
+
+    data() {
+        return {
+            email: '',
+            password: '',
+            passwordError: ''
+        }
+    },
+methods: {
+    handleSubmit() {
+            //Password have to be at least 8 characters
+            this.passwordError = this.password.length > 7 ?
+            '' : 'Password should be more than 8 characters long!';
+
+            //Password should be less than 15 characters
+            this.passwordError2 = this.password.length < 15 ?
+            '' : 'Password should be less than 15 characters!';
+
+            //Includes at least two lowercase alphabet characters
+            
+
+            //Includes at least one uppercase alphabet character
+            //Includes at least one numeric value
+
+            //It should start with an uppercase alphabet
+            this.passwordError6 = this.password[0].toUpperCase() === this.password[0] ?
+            '' : 'Password should start with an uppercase alphabet!';
+
+            //It should include the character "_"
+            
+
+            if(!this.passwordError) {
+                console.log(this.email);
+                console.log(this.password);
+            }
+        }
+    }
 }
+
+
 
 </script>
